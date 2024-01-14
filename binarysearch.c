984dd99f3789a7afc8ecf83050c55b9740534d4c
shaneVR097 <121772441+shaneVR097@users.noreply.github.com>
2024-01-14 19:02:43 +0530

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
 	int n,i,j,x,mid,f=0,p;
-	printf("\n Enter size of array : ");
+	printf("\n Enter size of the Sorted-array : ");
 	scanf("%d",&n);
 	int arr[n];
-	printf("\n Enter %d elements into the array : ",n);
+	printf("\n Enter %d elements into the Sorted-array : ",n);
 	for(i=0;i<n;i++)
 		scanf("%d",&arr[i]);
 	printf("\n Enter search element : ");
@@ -31,7 +32,7 @@ int main()
 	if(f==0) 
         printf("\n No such element found having value %d",x);
     else
-		printf("\n Element %d found at position: %d ",x,p);    	
-    
+		printf("\n Element %d found at position: %d ",x,p+1);    	
+		
+ 	getch();
 }
-
